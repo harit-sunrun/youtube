d7643c9f5cd204836819441753f3321fbdc8d64f
hhimanshu <harit.himanshu@yahoo.in>
2013-09-07 16:18:26 -0700

    trying to load application on deploy

--- app.py
+++ app.py
@@ -4,21 +4,23 @@ import os
 import sys
 
 try:
-   zvirtenv = os.path.join(os.environ['OPENSHIFT_PYTHON_DIR'],
-                           'virtenv', 'bin', 'activate_this.py')
-   execfile(zvirtenv, dict(__file__ = zvirtenv) )
+    zvirtenv = os.path.join(os.environ['OPENSHIFT_PYTHON_DIR'],
+                            'virtenv', 'bin', 'activate_this.py')
+    execfile(zvirtenv, dict(__file__=zvirtenv))
 except IOError:
-   pass
+    pass
 
 
 def run_gevent_server(app, ip, port=8080):
-   from gevent.pywsgi import WSGIServer
-   WSGIServer((ip, port), app).serve_forever()
+    from gevent.pywsgi import WSGIServer
+
+    WSGIServer((ip, port), app).serve_forever()
 
 
 def run_simple_httpd_server(app, ip, port=8080):
-   from wsgiref.simple_server import make_server
-   make_server(ip, port, app).serve_forever()
+    from wsgiref.simple_server import make_server
+
+    make_server(ip, port, app).serve_forever()
 
 
 #
@@ -31,15 +33,19 @@ def run_simple_httpd_server(app, ip, port=8080):
 #  main():
 #
 if __name__ == '__main__':
-   ip   = os.environ['OPENSHIFT_PYTHON_IP']
-   port = int(os.environ['OPENSHIFT_PYTHON_PORT'])
-   zapp = imp.load_source('application', 'wsgi/application')
+    ip = os.environ['OPENSHIFT_PYTHON_IP']
+    port = int(os.environ['OPENSHIFT_PYTHON_PORT'])
 
-   #  Use gevent if we have it, otherwise run a simple httpd server.
-   print 'Starting WSGIServer on %s:%d ... ' % (ip, port)
-   try:
-      run_gevent_server(zapp.application, ip, port)
-   except:
-      print 'gevent probably not installed - using default simple server ...'
-      run_simple_httpd_server(zapp.application, ip, port)
+    import django.core.handlers.wsgi
+
+    application = django.core.handlers.wsgi.WSGIHandler()
+    # zapp = imp.load_source('application', 'wsgi/application')
+
+    #  Use gevent if we have it, otherwise run a simple httpd server.
+    # print 'Starting WSGIServer on %s:%d ... ' % (ip, port)
+    # try:
+    #    run_gevent_server(zapp.application, ip, port)
+    # except:
+    #    print 'gevent probably not installed - using default simple server ...'
+    #    run_simple_httpd_server(zapp.application, ip, port)
 
