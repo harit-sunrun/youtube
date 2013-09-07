9fce6452904404a289fc7cd8c262efe2c4fece8f
hhimanshu <harit.himanshu@yahoo.in>
2013-09-07 15:41:21 -0700

    adding production configuration for database

--- setup.py
+++ setup.py
@@ -7,9 +7,5 @@ setup(name='YourAppName', version='1.0',
 
       #  Uncomment one or more lines below in the install_requires section
       #  for the specific client drivers/modules your application needs.
-      install_requires=['greenlet', 'gevent',
-                        #  'MySQL-python',
-                        #  'pymongo',
-                        #  'psycopg2',
-      ],
+      install_requires=['Django<=1.4', 'psycopg2', 'django-social-auth'],
      )
